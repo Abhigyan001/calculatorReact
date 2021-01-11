d7e55b7e8bad8253220e7941ee972c44b6547a6d
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-01-11 21:07:16 +0530

    Made changes to display.js to remove extra element

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,10 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Display = ({ result }) => (
-  <div id="display">
-    <div className="row">
-      <p className="result">{result}</p>
-    </div>
+  <div id="display">    
+    <p className="result">{result}</p>    
   </div>
 );
 
