bee7b41175a23c7e2137611b2a0cd5251985efe5
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-01-11 16:22:51 +0530

    Configure App.js to render Display and ButtonPanel using react fragments

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+// import Display from './Display';
+// import ButtonPanel from './ButtonPanel';
 
-function App() {
-  return (
-    <div>
-      <h1 id="head">Welcome to the calculator app!</h1>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <div>Check</div>
+
+    {/* <Display />
+    <ButtonPanel /> */}
+  </>
+);
 
 export default App;
